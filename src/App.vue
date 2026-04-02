e27d207a4d56f81642fd015106de596086946c56
xh <im0k@outlook.com>
2026-04-02 21:45:54 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="qa-container">
+    <div class="chat-container" style="padding: 0 10px">
+      <div class="title-bg" style="height:50px"></div>
+      <div class="qa-right-scrollbar">
+        <div class="message-list" v-for="item in list" :key="item">
+          <div class="talk-item">我发送的---{{ item }}</div>
+          <div class="talk-item">机器人回复的---{{ item }}</div>
+        </div>
+      </div>
+      <div class="message-input-chat">
+        <div class="roll-scroll">横向滚动按钮</div>
+        <div class="chat-box">输入框</div>
+        <div class="box-tip">内容由AI生成提示</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      list: [1, 2, 3, 4, 5, 6,7,8,9,10,11,12,13,14]
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.list.push(this.list.length + 1)
+    }, 200)
   }
 }
 </script>
 
 <style>
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  margin: 0;
+  ;
+}
+
+.qa-container {
+  height: 100%;
+  overflow: hidden;
+
+
+  .chat-container {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+
+    .title-bg {
+      background: #000;
+    }
+
+    .qa-right-scrollbar{
+      flex:1;
+      overflow-y: auto;
+    }
+
+    .message-input-chat{
+      height: 100px;
+      background: #ccc;
+    }
+  }
+
 }
 </style>
